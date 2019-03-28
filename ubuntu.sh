33c62ffac79999e89d3b65a841c5135859b6bfc6
strikersix23 <10016489@discoverers.org>
2019-03-28 08:56:06 -0500

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -18,7 +18,7 @@ if [ "$first" != 1 ];then
         elif [ "$(dpkg --print-architecture)" = "x86" ];then
             wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/disco-base-i386.tar.gz -O ubuntu.tar.gz
         elif [ "$(dpkg --print-architecture)" = "amd64" ];then
-            wget http://cdimage.ubuntu.com/ubuntu-base/releases/18.10/release/ubuntu-base-18.10-base-amd64.tar.gz -O ubuntu.tar.gz
+            wget http://cdimage.ubuntu.com/ubuntu-base/daily/current/disco-base-amd64.tar.gz -O ubuntu.tar.gz
 
 
 
