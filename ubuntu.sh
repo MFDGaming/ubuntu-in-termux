17639df07014a2f48a35a6edebd9174a2d5ce950
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-11-16 00:43:21 +0200

    Updated to ubuntu 20.10

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ time1="$( date +"%r" )"
 
 install1 () {
 directory=ubuntu-fs
-UBUNTU_VERSION=20.04.1
+UBUNTU_VERSION=20.10
 if [ -d "$directory" ];then
 first=1
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;227m[WARNING]:\e[0m \x1b[38;5;87m Skipping the download and the extraction\n"
