9a721a0866a90fe47fa74fb5f5fc46d7cea158db
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-12-02 22:46:41 +0200

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,7 +5,7 @@ time1="$( date +"%r" )"
 
 install1 () {
 directory=ubuntu-fs
-UBUNTU_VERSION=19.10
+UBUNTU_VERSION=18.04
 if [ -d "$directory" ];then
 first=1
 printf "\x1b[38;5;214m[${time1}]\e[0m \x1b[38;5;227m[WARNING]:\e[0m \x1b[38;5;87m Skipping the download and the extraction\n"
